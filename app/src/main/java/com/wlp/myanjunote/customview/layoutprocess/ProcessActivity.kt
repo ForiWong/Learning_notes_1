e2912b0d759698d33a83a55a57776e5d39a7796c
wanglp2019 <xb80230218@163.com>
2023-05-23 15:56:43 +0800

    sample view

--- app/src/main/java/com/wlp/myanjunote/customview/layoutprocess/ProcessActivity.kt
+++ app/src/main/java/com/wlp/myanjunote/customview/layoutprocess/ProcessActivity.kt
@@ -1,13 +1,39 @@
 package com.wlp.myanjunote.customview.layoutprocess
 
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.ObjectAnimator
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.wlp.myanjunote.R
+import com.wlp.myanjunote.customview.sample.CircleProgress
+import com.wlp.myanjunote.customview.sample.GoodProgressView
+import com.wlp.myanjunote.customview.sample.MyTextView
+import java.util.*
 
 class ProcessActivity : AppCompatActivity() {
+    private lateinit var progress1: GoodProgressView
+    private lateinit var progress2: GoodProgressView
+    var mRandom = Random()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_process)
+
+        val mt = findViewById<MyTextView>(R.id.myTextView)
+        ObjectAnimator.ofFloat(mt, "progress", 0f, 1f).setDuration(5000).start()
+
+        progress1 = findViewById(R.id.good_progress_view1)
+        progress2 = findViewById(R.id.good_progress_view2)
+        progress1.setProgressValue(90)
+        progress1.setColors(intArrayOf(Color.RED, Color.MAGENTA))
+        progress2.setProgressValue(60)
+        progress2.setColors(intArrayOf(Color.RED, Color.MAGENTA))
+
+        val mCircleProgress = findViewById<CircleProgress>(R.id.circle_progress)
+        mCircleProgress.setOnClickListener(View.OnClickListener {
+            mCircleProgress.setValue(mRandom.nextFloat() * mCircleProgress.getMaxValue());
+        })
     }
 }
 
@@ -36,4 +62,4 @@ com.wlp.myanjunote D/K1View: onLayout
 com.wlp.myanjunote D/K2View: onDraw
 com.wlp.myanjunote D/K3View: onDraw
 com.wlp.myanjunote D/K1View: onDraw
-**/
+ **/
---
+++ app/src/main/java/com/wlp/myanjunote/customview/sample/MyTextView.java
@@ -0,0 +1,76 @@
+package com.wlp.myanjunote.customview.sample;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class MyTextView extends View {
+    private float progress = 0.0f;
+
+    public float getProgress() {
+        return progress;
+    }
+
+    public void setProgress(float progress) {
+        //invalidate();
+        this.progress = progress;
+        //Log.v("dd","progress -》"+ progress);
+        invalidate();
+    }
+
+    public MyTextView(Context context) {
+        super(context);
+    }
+
+    public MyTextView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyTextView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    String text = "你好啊,欢迎阅览我的博客";
+
+    private float TopRight;
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        Paint paint = new Paint();
+        super.onDraw(canvas);
+        Log.v("zj", "ondraw");
+        paint.setTextSize(100);
+        paint.setAntiAlias(true);//抗锯齿
+        paint.setStyle(Paint.Style.FILL);
+        Paint.FontMetrics fm = paint.getFontMetrics();
+        canvas.save();
+
+        float drLeft = getWidth() / 2 - paint.measureText(text) / 2;
+        canvas.drawLine(0, getHeight() / 2, getWidth(), getHeight() / 2, paint);
+        canvas.drawLine(getWidth() / 2, 0, getWidth() / 2, getHeight(), paint);
+        canvas.drawText(text, drLeft, getHeight() / 2 - (fm.descent + fm.ascent) / 2, paint);
+
+        canvas.restore();
+
+        canvas.save();
+        drLeft = getWidth() / 2 - paint.measureText(text) / 2;
+        TopRight = (float) (drLeft + progress * paint.measureText(text));
+        paint.setColor(Color.RED);
+        Rect rect = new Rect((int) drLeft, 0, (int) TopRight, getHeight());
+        canvas.clipRect(rect);
+        canvas.drawText(text, drLeft, getHeight() / 2 - (fm.descent + fm.ascent) / 2, paint);
+        canvas.restore();
+    }
+}
